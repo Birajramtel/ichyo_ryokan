184e78a1790f771265a40b024ccb168dd4036664
Birajramtel <biraj.ramtel007@gmail.com>
2021-10-22 11:30:30 +0545

    Changes made in Footer page with all contacts and Dead links managed

--- include/footer.php
+++ include/footer.php
@@ -8,18 +8,18 @@
                     culture and cuisine.
                 </p>
                 <div class="footer-social-icon mt-2">
-                    <a href="https://www.facebook.com/Naava-Resort-Retreat-100547551927011/"><i class="fab fa-facebook-f facebook-bg"></i></a>
-                    <a href="#"><i class="fab fa-twitter twitter-bg"></i></a>
-                    <a href="#"><i class="fab fa-google-plus-g google-bg"></i></a>
+                    <a href="https://www.facebook.com/ichyoryokansanga"><i class="fab fa-facebook-f facebook-bg"></i></a>
+                    <a href="https://www.facebook.com/ichyoryokansanga"><i class="fab fa-twitter twitter-bg"></i></a>
+                    <a href="https://www.facebook.com/ichyoryokansanga"><i class="fab fa-google-plus-g google-bg"></i></a>
                 </div>
             </div>
             <div class="col-md-3 col-sm-6 col-xm-12 mt-3">
                 <h2 class="text-white">Useful Links</h2>
                 <ul style="list-style-type:none;">
-                    <li><a class="footer_link" href="#">About Us</a></li>
-                    <li><a class="footer_link" href="package.php">Rooms</a></li>
-                    <li><a class="footer_link" href="#">Aminities</a></li>
-                    <li><a class="footer_link" href="#">Gallary</a></li>
+                    <li><a class="footer_link" href="about.php">About Us</a></li>
+                    <li><a class="footer_link" href="highlight.php">Rooms</a></li>
+                    <li><a class="footer_link" href="highlight.php">Aminities</a></li>
+                    <li><a class="footer_link" href="gallery.php">Gallary</a></li>
                 </ul>
             </div>
             <div class="col-md-3 col-sm-6 col-xm-12 mt-3">
@@ -27,20 +27,20 @@
                 <ul style="list-style-type: none;">
                     <li><a class="footer_link" href="#">Career</a></li>
                     <li><a class="footer_link" href="contact.php">Privacy Policy</a></li>
-                    <li><a class="footer_link" href="#">Contact Us</a></li>
+                    <li><a class="footer_link" href="contact.php">Contact Us</a></li>
                 </ul>
             </div>
             <div class="col-md-3 col-sm-6 col-xm-12 mt-3">
                 <h2 class="text-white">Contact</h2>
                 <div class="footer-social-icon">
                     <div class="single-cta">
-                        <i class="far fa-envelope-open"></i>&nbsp;<span class="text-white">example@ichyo.com</span>
+                        <i class="far fa-envelope-open"></i>&nbsp;<span class="text-white">minatamanglama@gmail.com</span>
                     </div>
                     <div class="single-cta">
-                        <i class="fas fa-phone-alt"></i>&nbsp;<span class="text-white">+0 320 422 4254</span>
+                        <i class="fas fa-phone-alt"></i>&nbsp;<span class="text-white"><a href="tel:011-660864">011-660864</a> </span>
                     </div>
                     <div class="single-cta">
-                        <i class="fas fa-map-marker-alt"></i>&nbsp;<span class="text-white">Main Str Nepal</span>
+                        <i class="fas fa-map-marker-alt"></i>&nbsp;<span class="text-white">Sanga-Ashapuri marg</span>
                     </div>
                 </div>
             </div>
